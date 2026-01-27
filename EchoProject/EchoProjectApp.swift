e5f0a50bb294dbdc61d0c29a4408e14ef66b4cf9
Jay Chauhan <jayschauhan3@gmail.com>
2026-01-27 13:21:29 -0500

    Initial Commit

---
+++ EchoProject/EchoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoProjectApp.swift
+//  EchoProject
+//
+//  Created by Jay Chauhan on 1/27/26.
+//
+
+import SwiftUI
+
+@main
+struct EchoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
